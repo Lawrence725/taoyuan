151a17e7a574c898f73f8726ec76fc30ec1cee99
Lawrence <lawrence.0725@hotmail.com>
2022-02-14 16:10:12 +0800

    Update result

--- lottery/index.js
+++ lottery/index.js
@@ -1,3 +1,20 @@
+// All Popup (Click outside popup close)
+window.onclick = function(event) {
+    if (event.target == popResult) {
+        popResult.style.display = "none"; }}
+
+// Popup (Result)
+var popResult    = document.getElementById("popResult");
+var buttonResult = document.getElementById("buttonResult");
+buttonResult.onclick = function() { popResult.style.display = "flex"; }
+
 function ani() {
+    document.getElementById('pockets').className = 'hide pockets itemH width height middle';
+    document.getElementById('buttons').className = 'hide buttons itemH width middle';
     document.getElementById('pocketH').className = 'animation pocketH itemH height middle';
-}
+    setTimeout( function () { 
+        popResult.style.display = "flex";
+        document.getElementById('pockets').className = 'pockets itemH width height middle';
+        document.getElementById('buttons').className = 'buttons itemH width middle';
+        document.getElementById('pocketH').className = 'pocketH itemH height middle';
+    }, 1500 ); }
