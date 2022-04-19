5ae775867b6d4752b94ceebb7a6aa3a5ccb09cc2
Lawrence <lawrence.0725@hotmail.com>
2022-04-19 17:07:09 +0800

    Update 掃描頁 (上滑區塊改成popup)

---
+++ app/js/scan.js
@@ -0,0 +1,13 @@
+// Popup (Code)
+function popCode() {
+    var popCode     = document.getElementById("popCode");
+    var close       = popCode.getElementsByClassName("close")[0];
+    var submit      = popCode.getElementsByClassName("submit")[0];
+    close.onclick   = function() { popClose(); }
+    submit.onclick  = function() { popClose(); }
+    window.onclick  = function(event) { if (event.target == popCode) { popClose(); }}
+    function popClose() { 
+        popCode.style.display = "none";
+        document.body.style.overflow = "scroll"; }
+    popCode.style.display = "flex";
+    document.body.style.overflow = "hidden"; }
