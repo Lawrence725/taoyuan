400076ebdc5ac2d82f71cb2d6c803c1c54aae45c
Lawrence <lawrence.0725@hotmail.com>
2022-04-27 12:48:59 +0800

    Update 首頁 (Popup: 獲得點數, 無問答)

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,22 @@
-// Popup (Activity)
-function popActivity() {
-    var popActivity = document.getElementById("popActivity");
-    var close       = popActivity.getElementsByClassName("close")[0];
-    close.onclick   = function() { popClose(); }
-    window.onclick  = function(event) { if (event.target == popActivity) { popClose(); }}
-    function popClose() { 
-        popActivity.style.display = "none";
-        document.body.style.overflow = "scroll"; }
-    popActivity.style.display = "flex";
+// Popup close
+function popClose(e) {
+    e.closest('.popup').style.display = "none";
+    document.body.style.overflow = "scroll"; }
+
+// Popup background close
+function popBgClose(e) {
+    window.onclick  = function(event) { if (event.target == e) {
+        e.style.display = "none";
+        document.body.style.overflow = "scroll"; }}}
+
+// Popup (獲得點數)
+function popPoint() {
+    document.getElementById("popPoint").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
+
+// Popup (無問答)
+function popNoQA() {
+    document.getElementById("popNoQA").style.display = "flex";
     document.body.style.overflow = "hidden"; }
 
 // Slideshow
