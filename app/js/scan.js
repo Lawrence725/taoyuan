0b85a699256ce99d52b1ff135519db001e7055ff
Lawrence <lawrence.0725@hotmail.com>
2022-04-29 21:42:10 +0800

    Update

--- app/js/scan.js
+++ app/js/scan.js
@@ -2,9 +2,9 @@
 function popCode() {
     var popCode     = document.getElementById("popCode");
     var close       = popCode.getElementsByClassName("close")[0];
-    var submit      = popCode.getElementsByClassName("submit")[0];
+    // var submit      = popCode.getElementsByClassName("submit")[0];
     close.onclick   = function() { popClose(); }
-    submit.onclick  = function() { popClose(); }
+    // submit.onclick  = function() { popClose(); }
     window.onclick  = function(event) { if (event.target == popCode) { popClose(); }}
     function popClose() { 
         popCode.style.display = "none";
