9424ffe7dffbf7ea5690896027bc2483ffd3f5a1
Lawrence <lawrence.0725@hotmail.com>
2022-06-14 18:26:09 +0800

    Add 集章內頁

--- app/js/stamp.js
+++ app/js/stamp.js
@@ -13,7 +13,22 @@ function popCode() {
     document.getElementById("popCode").style.display = "flex";
     document.body.style.overflow = "hidden"; }
 
+// Popup (Wallet)
+function popWallet() {
+    document.getElementById("popWallet").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
+
 // Popup (History)
 function popHistory() {
     document.getElementById("popHistory").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
+
+// Popup (Gift)
+function popGift() {
+    document.getElementById("popGift").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
+
+// Popup (GiftGet)
+function popGiftGet() {
+    document.getElementById("popGiftGet").style.display = "flex";
     document.body.style.overflow = "hidden"; }
