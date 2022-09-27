a8e0ff29bac5653b9c0ab01837484e5f1e8c8498
Lawrence <lawrence.0725@hotmail.com>
2022-09-27 14:36:10 +0800

    Add 交易明細

--- app/js/point_history.js
+++ app/js/point_history.js
@@ -1,3 +1,18 @@
+// Popup close
+function popClose(e) {
+    if (e.classList.contains('popup'))
+        window.onclick  = function(event) {
+            if (event.target == e)
+                e.style.display = "none"; }
+    else if (!(e.classList.contains('popBody')))
+        e.closest('.popup').style.display = "none";
+    document.body.style.overflow = "scroll"; }
+
+// Popup (獲得點數)
+function popDetail() {
+    document.getElementById("popDetail").style.display = "flex";
+    document.body.style.overflow = "hidden"; }
+
 // Popup (Time)
 function popTime() {
     var popTime     = document.getElementById("popTime");
